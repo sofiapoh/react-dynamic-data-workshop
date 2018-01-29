351c68d071c7dab1d6f26be7904fda0be845ba5d
Sofia Pohjalainen <sofia.pohjalainen@ticketmaster.co.uk>
2018-01-29 15:14:38 +0000

    clean up

--- src/components/card/card_image/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-export const CardImage = ({ url }) => (
-  <figure className="card-image--container">
-    <img className="card-image--img" src={url} />
-  </figure>
-);
