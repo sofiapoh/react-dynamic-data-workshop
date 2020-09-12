d08d87a20c2addd16356948afd8f94c434a7d8ab
Oliver Phillips <oliverjamesphillips@gmail.com>
2020-09-12 10:49:57 +0100

    Refactor workshop/ to use CRA instead of Parcel
    Mostly just adding the react-scripts dependency.
    
    Also copied over the index.html

--- workshop/src/components/App.js
+++ workshop/src/components/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-export default function App() {
+function App() {
   const [placeholder, setPlaceholder] = React.useState(
     "Let's get started! You can delete this state as you won't need it going forward."
   );
   return <div>{placeholder}</div>;
 }
+
+export default App;
